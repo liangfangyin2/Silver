12391fa2b28e07a814d8f1c36550ca9e47a32cb9
liangfangyin <550762978@qq.com>
2024-07-23 15:10:39 +0800

    初次提交

---
+++ Silver/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using Silver.Basic;
+using Silver.ViewFace;
+
+ 
+var result= ViewFaceUtil.GetFaceBase("D:\\imgs\\14.jpg");
+
+Console.WriteLine((new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds()).ToString());
+Console.ReadLine();
+
